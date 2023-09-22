c0445fec0d0874d3c516fe58e1491592ed7775f8
wukongdaily <2666180@gmail.com>
2023-09-22 22:59:35 +0800

    增加prepare.sh
    如果要运行更多函数 建议先执行它

--- drive-gl-inet.sh
+++ drive-gl-inet.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-opkg update
-opkg install bash
+check_bash_installed() {
+  if [ -x "/bin/bash" ]; then
+    echo "downloading gl-inet.sh ......"
+  else
+    opkg update
+    opkg install bash
+  fi
+}
+check_bash_installed
 wget -O /tmp/gl-inet.sh https://ghproxy.com/https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/gl-inet.sh && chmod +x /tmp/gl-inet.sh && /tmp/gl-inet.sh
---
+++ prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 默认情况下MT3000和MT2500A的环境是ash
+# 建议安装bash 满足更多的函数调用
+check_bash_installed() {
+  if [ -x "/bin/bash" ]; then
+    echo "downloading gl-inet.sh ......"
+  else
+    opkg update
+    opkg install bash
+  fi
+}
+check_bash_installed
