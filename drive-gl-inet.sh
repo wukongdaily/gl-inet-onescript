d645c791470dc7a1d05bc7c567d2e1e895a341c7
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2023-11-07 08:16:10 +0800

    Update drive-gl-inet.sh

--- drive-gl-inet.sh
+++ drive-gl-inet.sh
@@ -8,4 +8,4 @@ check_bash_installed() {
   fi
 }
 check_bash_installed
-wget -O /tmp/gl-inet.sh https://ghproxy.com/https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/gl-inet.sh && chmod +x /tmp/gl-inet.sh && /tmp/gl-inet.sh
+wget -O /tmp/gl-inet.sh https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/gl-inet.sh && chmod +x /tmp/gl-inet.sh && /tmp/gl-inet.sh
