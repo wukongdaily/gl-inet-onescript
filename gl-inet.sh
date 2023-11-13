b4856da740690b5e4894ac23ee0d2d7ee7f2e337
wukongdaily <2666180@gmail.com>
2023-11-13 10:30:41 +0800

    Update gl-inet.sh
    check bash env

--- gl-inet.sh
+++ gl-inet.sh
@@ -260,10 +260,17 @@ set_glfan_temp() {
 		echo "错误: 请输入整数."
 	fi
 }
+check_bash_installed() {
+  if [ -x "/bin/bash" ]; then
+    echo "rollback_old_version ......"
+  else
+    setup_software_source 0
+    opkg install bash
+  fi
+}
 
 rollback_old_version() {
-	setup_software_source 0
-	opkg install bash
+	check_bash_installed
 	download_url="https://github.com/wukongdaily/gl-inet-onescript/raw/1f25c161512e9b416227f60656e8c2139c993f69/gl-inet.run"
 	local_file_path="/tmp/gl-inet.run"
 	wget -O "$local_file_path" "$download_url"
